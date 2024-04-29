412098f849f8b8d5bd85860bba3d930081e24463
ethan <20230006@ricaldone.edu.sv>
2024-04-29 17:59:46 -0600

    subi la app

---
+++ app/src/main/java/ethan/herniquez/emulador_20230006_ethan/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package ethan.herniquez.emulador_20230006_ethan.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
