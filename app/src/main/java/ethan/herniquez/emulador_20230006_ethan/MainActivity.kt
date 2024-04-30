0ed9d410d5496d5525d25c8c17190e2c2eee2c91
ethan <20230006@ricaldone.edu.sv>
2024-04-29 19:23:10 -0600

    agregue cosas

--- app/src/main/java/ethan/herniquez/emulador_20230006_ethan/MainActivity.kt
+++ app/src/main/java/ethan/herniquez/emulador_20230006_ethan/MainActivity.kt
@@ -1,6 +1,9 @@
 package ethan.herniquez.emulador_20230006_ethan
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
@@ -15,7 +18,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
@@ -28,5 +31,14 @@ class MainActivity : AppCompatActivity() {
                 R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications))
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
     }
+    fun onClick(view: View){
+        Toast.makeText( this, "Hola soy Ethan David Henriquez Díaz", Toast.LENGTH_LONG).show()
+    }
+
+
+
+
+
 }
